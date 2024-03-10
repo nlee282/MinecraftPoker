1597ae3dca52f3e5902a855ac40c07db796ac8b2
nlee282 <nlee282@gmail.com>
2024-03-10 15:20:13 -0400

    initial

---
+++ src/main/java/me/poker/Poker.java
@@ -0,0 +1,17 @@
+package me.poker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Poker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
